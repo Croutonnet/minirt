614b4f572fe929528099ce73a8cf8517a291e04f
Raphael Pelchat <rapelcha@c2r5p6.42quebec.com>
2023-11-15 13:29:51 -0500

    before branch

--- include/image.h
+++ include/image.h
@@ -1,11 +1,14 @@
 #ifndef IMAGE_H
 # define IMAGE_H
 
+#include "vector.h"
 typedef struct s_viewport
 {
-	double	width;
-	double	height;
-	double	ratio;
+	double		width;
+	double		height;
+	double		ratio;
+	t_vector	*v;
+	t_vector	*u;
 }			t_viewport;
 
 
--- src/main.c
+++ src/main.c
@@ -15,8 +15,9 @@ int	main(void)
 	viewport.ratio = 16.0 / 9.0;
 	viewport.width = 300;
 	viewport.height = viewport.width / viewport.ratio;
-	printf("%f\n", viewport.height);
 	mlx = mlx_init(1920, 1080, "coucou", true);
+	// create_vector(viewport.u, viewport.width, 0, 0);
+	// create_vector(viewport.v, viewport.width, 0, 0);
 	image = mlx_new_image(mlx, viewport.width, viewport.height);
 	for (int w = 0; w < viewport.width; w++){
 		for (int h = 0; h < (int)viewport.height; h++){
