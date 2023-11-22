7057093bb1ba46d6f19193427cef64cb1ccb6da7
Bruno-Pierre <BrunoPierreBouchard@hotmail.ca>
2023-11-22 17:45:55 -0500

    demo 4 sphere

--- src/main.c
+++ src/main.c
@@ -51,8 +51,10 @@ int	main(void)
 	t_shapes_arr shapes;
 	shapes.count = 0;
 
-	add_shape(&shapes, SPHERE, (u_geom)create_sphere(0, 0, -10, 0.5));
-	add_shape(&shapes, SPHERE, (u_geom)create_sphere(10, 0, -10, 0.5));
+	add_shape(&shapes, SPHERE, (u_geom)create_sphere(0, 0, -10, 3));
+	add_shape(&shapes, SPHERE, (u_geom)create_sphere(10, 0, -10, 0.2));
+	add_shape(&shapes, SPHERE, (u_geom)create_sphere(0, 10, -10, 1));
+	add_shape(&shapes, SPHERE, (u_geom)create_sphere(0, -10, -10, 0.5));
 
 	viewport.ratio = IMAGE_WIDTH / IMAGE_HEIGHT;
 	viewport.height = 2.0;
