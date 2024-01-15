22bc54dd2d6c8351bb091ad877bb0eb33fd20519
Bruno <BrunoPierreBouchard@hotmail.ca>
2024-01-15 14:28:54 -0500

    normer sphere.c

--- src/sphere.c
+++ src/sphere.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sphere.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbouchar <bbouchar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bbouchar <BrunoPierreBouchard@hotmail.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:24:45 by bbouchar          #+#    #+#             */
-/*   Updated: 2024/01/10 13:24:47 by bbouchar         ###   ########.fr       */
+/*   Updated: 2024/01/15 14:27:21 by bbouchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,41 +32,56 @@ t_sphere	create_sphere(t_vector pPosition, float radius, t_color pColor)
 	return (s);
 }
 
+//float t1 = (-b + sqrt(dis))/2.0f;
+// calcule la valeur de t
+// calcule points de collision
+// x,y,z entre -1 et 1
+// si un object intersecte lightDir, alors le pixel est une ombre...
+static t_color	light(t_sphere s, t_ray *r, float t2, t_data *data)
+{
+	t_vector	normal;
+	t_vector	lightdir;
+	t_color		color;
+	float		intensity;
+
+	normal = normalize(minus_vec(get_ray_point(*r, t2), s.origin));
+	lightdir = normalize(minus_vec(data->light.origin, s.origin));
+	intensity = dot_vec(normal, lightdir) / 2;
+	if (intensity < 0)
+		intensity = 0;
+	color.x = (data->alight.intensity * data->alight.color.x)
+		+ (intensity * s.base_color.x * data->light.color.x);
+	color.y = (data->alight.intensity * data->alight.color.y)
+		+ (intensity * s.base_color.y * data->light.color.y);
+	color.z = (data->alight.intensity * data->alight.color.z)
+		+ (intensity * s.base_color.z * data->light.color.z);
+	r->t = t2;
+	r->hit = true;
+	return (color);
+}
+
 // If sphere intersect ray
 // return a color and put r.hit = true, retunr intersection point.
-t_vector	sphere_intersect_ray(t_sphere s, t_ray *r, t_data *data)
+// oc = Direction vers le centre de la sphere
+// dis = Discriminant < 0 si rien toucher, 
+// 0 toucher une fois, 1 toucher deux fois
+t_color	sphere_intersect_ray(t_sphere s, t_ray *r, t_data *data)
 {
-	t_color	color;
-	color.x = 0;
-	color.y = 0;
-	color.z = 0;
+	t_vector	oc;
+	float		b;
+	float		c;
+	float		dis;
+	float		t2;
 
-	t_vector	oc = minus_vec(r->origin_point, s.origin); //Direction vers le centre de la sphere
-	float		b = 2.0 * dot_vec(minus_vec(r->direction,r->origin_point), oc);
-	float		c = pow(length_vec(oc), 2) - pow(s.radius, 2); //
-	float		dis = pow(b, 2) - (4 * c); //Discriminant < 0 si rien toucher, 0 toucher une fois, 1 toucher deux fois
+	oc = minus_vec(r->origin_point, s.origin);
+	b = 2.0 * dot_vec(minus_vec(r->direction, r->origin_point), oc);
+	c = pow(length_vec(oc), 2) - pow(s.radius, 2);
+	dis = pow(b, 2) - (4 * c);
 
 	if (dis >= 0)
 	{
-		//float t1 = (-b + sqrt(dis))/2.0f;
-		float t2 = (-b - sqrtf(dis))/(2.0f); // calcule la valeur de t
-		t_vector h2 = get_ray_point(*r, t2); // calcule points de collision
-		// calcul normal
-		t_vector normal = normalize(minus_vec(h2, s.origin)); // x,y,z entre -1 et 1
-		// // calcul lumiere
-		t_vector lightDir = normalize(minus_vec(data->light.origin, s.origin));
-		// si un object intersecte lightDir, alors le pixel est une ombre...
-
-
-		float intensity =  dot_vec(normal, lightDir) / 2;
-		if (intensity < 0)
-			intensity = 0;
-        color.x = (data->alight.intensity * data->alight.color.x) + (intensity * s.base_color.x * data->light.color.x);
-        color.y = (data->alight.intensity * data->alight.color.y) + (intensity * s.base_color.y * data->light.color.y);
-        color.z = (data->alight.intensity * data->alight.color.z) + (intensity * s.base_color.z * data->light.color.z);
-		r->t = t2;
-		r->hit = true;
-		return color;
+		t2 = (-b - sqrtf(dis)) / (2.0f);
+		return (light(s, r, t2, data));
 	}
-	return color;
+	return ((t_color){0, 0, 0});
 }
